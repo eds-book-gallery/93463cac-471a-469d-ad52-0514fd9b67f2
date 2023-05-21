30a8e446ab73642fab8b09a12287fa5c48d12f2c
Owen <owen.allemang@gmail.com>
2023-05-21 02:48:59 +0100

    added os to manage some paths

--- pub_scripts/article_analysis_inver.py
+++ pub_scripts/article_analysis_inver.py
@@ -1,10 +1,12 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats.kde import gaussian_kde
 from numpy import linspace
 import extraction_data_2 as extr
 
-ALL_data = np.load('figures/ALL/inver.npy')
+ALL_data = np.load(os.path.join('figures','ALL','inver.npy'))
 print(ALL_data.shape)
 data = np.sort(ALL_data[:, 2, 93])
 data = data[30:1200 - 30]

ce5c94e3219307be31d9d2b22775588ce6e33ff1
Owen Allemang <owen.allemang@gmail.com>
2023-05-21 16:39:30 +0100

    docstring

--- pub_scripts/inverse_model.py
+++ pub_scripts/inverse_model.py
@@ -7,12 +7,32 @@ from torch.autograd import Variable
 
 # fonction réalisant l'inversion variationelle
 def model_inverse(entry: torch.Tensor, target: torch.Tensor, model, alpha: float = 0.005) -> tuple:
+	"""
+	Performs model inverse optimization to modify the given input array to approximate the target array.
+
+	Parameters
+	----------
+	entry : torch.Tensor
+		Input array to be modified.
+	target : torch.Tensor
+		Target array to be approximated.
+	model : object
+		The model used for prediction.
+	alpha : float, optional
+		Threshold value for the loss, indicating the desired approximation level. Default is 0.005.
+
+	Returns
+	-------
+	tuple
+		Tuple containing the modified input array and the predicted array after optimization.
+	"""
+
 	criterion = nn.MSELoss()
 
 	X = Variable(entry.clone().detach(), requires_grad=True)
 	optimizer = torch.optim.Adam([X], lr=0.0001)
 
-	for i in range(100000):
+	for i in range(100_000):
 		current = model(X)
 		loss = criterion(current.float(), target.float()) + 0.01 * criterion(X.float(), entry.float())
 
@@ -20,8 +40,8 @@ def model_inverse(entry: torch.Tensor, target: torch.Tensor, model, alpha: float
 			print(
 				f"Iteration {i}:\n\tloss exit {criterion(current, target)}\n\tloss entry {criterion(entry, X)}"
 			)
-		loss.backward()
 
+		loss.backward()
 		optimizer.step()
 		optimizer.zero_grad()
 
